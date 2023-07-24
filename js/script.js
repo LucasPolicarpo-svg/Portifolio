e024a7a95c3558efcc22c0514d47c8ed6cade608
LucasPolicarpo-svg <empresariallrp@gmail.com>
2023-07-24 12:33:19 -0300

    feat add new module NUM(011)

--- js/script.js
+++ js/script.js
@@ -2,9 +2,23 @@ window.addEventListener('scroll' , function(){
     var header = document.querySelector('header');
     header.classList.toggle('sticky', window.scrollY > 0);});
 
-    function toggleMenu(){
-        var menuToggle = document.querySelector('.toggle');
-        var menu = document.querySelector('.menu');
-        menuToggle.classList.toggle('active');
-        menu.classList.toggle('active');
-    }
+
+function toggleMenu() {
+    const menu = document.querySelector('.menu');
+    menu.classList.toggle('active');
+}
+    
+function onClick() {
+    const menuLinks = document.querySelectorAll('.menu a');
+    const menu = document.querySelector('.menu');
+    menuLinks.forEach(link => {
+        link.addEventListener('click', () => {
+             menu.classList.remove('active');
+        });
+    });
+}
+    
+const menuToggle = document.getElementById('menu-toggle');
+menuToggle.addEventListener('click', toggleMenu);
+    
+onClick();
--- js/stars.js
+++ js/stars.js
@@ -1,54 +1,24 @@
-let intervalId;
 
-function createStars() {
-  const screenWidth = 1440; // Largura da tela
-  const numStars = Math.floor(screenWidth / 5); // Quantidade de bolinhas que cabem na tela
+    let starsContainer = document.getElementById('stars-container');
 
-  const starsContainer = document.getElementById('stars-container');
+    function createStar() {
+        const screenWidth = 1920;
+        const star = document.createElement('div');
+        star.classList.add('mini-stars');
 
-  // Limpa as bolinhas existentes antes de criar novas
-  starsContainer.innerHTML = '';
+        // Define uma posição aleatória para a bolinha na tela
+        const posX = Math.random() * screenWidth;
+        const posY = Math.random() * window.innerHeight;
 
-  for (let i = 0; i < numStars; i++) {
-    const star = document.createElement('div');
-    star.classList.add('mini-stars');
+        star.style.left = `${posX}px`;
+        star.style.top = `${posY}px`;
 
-    // Define uma posição aleatória para a bolinha na tela
-    const posX = Math.random() * screenWidth;
-    const posY = Math.random() * window.innerHeight;
+        starsContainer.appendChild(star);
+    }
 
-    star.style.left = `${posX}px`;
-    star.style.top = `${posY}px`;
+    function createStarsLoop() {
+        setInterval(createStar, 50);
+    }
 
-    starsContainer.appendChild(star);
-  }
-}
-
-// Função para criar bolinhas em loop infinito
-function createStarsLoop() {
-  createStars(); // Cria as bolinhas imediatamente
-
-  // Executa a função createStars() a cada 1000ms (1 segundo) e armazena o ID do intervalo
-  intervalId = setInterval(createStars, 1000);
-}
-
-// Função para parar o loop infinito de criação de bolinhas
-function stopStarsLoop() {
-  clearInterval(intervalId); // Limpa o intervalo
-}
-
-// Adiciona um ouvinte de evento para o botão
-document.getElementById('toggle-button').addEventListener('click', function () {
-  const buttonText = this.innerText.toLowerCase();
-
-  if (buttonText === 'toggle stars') {
-    createStarsLoop(); // Inicia o loop infinito
-    this.innerText = 'Stop Stars'; // Muda o texto do botão para "Stop Stars"
-  } else {
-    stopStarsLoop(); // Interrompe o loop infinito
-    this.innerText = 'Toggle Stars'; // Muda o texto do botão de volta para "Toggle Stars"
-  }
-});
-
-// Chama a função para criar bolinhas inicialmente ao carregar a página
-createStarsLoop();
+    // Chama a função para criar bolinhas inicialmente ao carregar a página
+    createStarsLoop();
